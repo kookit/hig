e4b6a9b970e320b6e8eee1c84b4522b511f303e1
Eric Fung <ericf@carbonfive.com>
2018-04-17 12:46:58 -0700

    chores(jest): Package version bump

--- support/jest/setupTests.js
+++ support/jest/setupTests.js
@@ -1,5 +1,9 @@
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-15";
 import "jest-enzyme";
 import "jest-extended";
 import "./matchers/toBeConstants";
 import "./matchers/toImplementHIGInterfaceOf";
 import "./matchers/toHavePropTypesAndDocgenInfo";
+
+configure({ adapter: new Adapter() });
